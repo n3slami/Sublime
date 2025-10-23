920f797523f88c23ad2b06b101d434d0261d47bd
n3slami <n3slami@gmail.com>
2025-10-22 21:15:03 -0400

    Remove extra baselines

--- bench/scripts/setup_includes.sh
+++ bench/scripts/setup_includes.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-git submodule update --init ../sketches_benchmark/include/bitsense/
-cd ../sketches_benchmark/include/bitsense/
-git submodule update --init simulator/third_party/eigen
-git submodule update --init simulator/third_party/fmt
-git submodule update --init simulator/third_party/toml
-sed -i '12i #include <cstdint>' ./simulator/src/common/hash.h
-sed -i '12i #include <time.h>' ./simulator/src/common/hash.h
-cd -
-
-git submodule update --init ../sketches_benchmark/include/sead_counter/
-
 git submodule update --init ../sketches_benchmark/include/waving_sketch/
 sed -i '9i #include <unistd.h>' ../sketches_benchmark/include/waving_sketch/include/Waving.h
 
